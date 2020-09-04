88648d05e7d548551a367969d0f72fc51e245f17
Sumudu Jayasinghe <sumudu.jayasinghe@texussolutions.com>
2020-09-04 09:23:01 +0530

    txt

--- packages/ui-widgets/src/atoms/button/index.tsx
+++ packages/ui-widgets/src/atoms/button/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
 
-const Button = (): ReactElement => <button type="submit">Buttons</button>;
+const Button = (): ReactElement => <button type="submit">Buttons...</button>;
 
 export default Button;
